e03387fa5af1c883067e45e0405a8ba647b60fbf
Иван Дружицкий <idruzhitskiy@mail.ru>
2021-07-14 13:53:00 +0000

    add default route

--- bob-management-server/src/main.rs
+++ bob-management-server/src/main.rs
@@ -3,15 +3,23 @@ extern crate rocket;
 #[macro_use]
 extern crate log;
 
+use rocket::response::Redirect;
 use rocket_dyn_templates::Template;
 
 mod routes;
 mod services;
 mod storages;
 
+#[get("/")]
+fn default() -> Redirect {
+    Redirect::to(uri!(routes::auth::login::get))
+}
+
 #[launch]
 fn rocket() -> _ {
+    let mut routes = routes::get_routes();
+    routes.extend(routes![default]);
     rocket::build()
-        .mount("/", routes::get_routes())
+        .mount("/", routes)
         .attach(Template::fairing())
 }
