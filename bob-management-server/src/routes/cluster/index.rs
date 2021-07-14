ee7ac81a88a9e56a355de592021af9b0aeb35293
Иван Дружицкий <idruzhitskiy@mail.ru>
2021-07-14 14:10:22 +0000

    add more cluster info

--- bob-management-server/src/routes/cluster/index.rs
+++ bob-management-server/src/routes/cluster/index.rs
@@ -5,16 +5,54 @@ use rocket::response::Redirect;
 use rocket_dyn_templates::Template;
 use serde::Serialize;
 
-#[derive(Serialize)]
+#[derive(Serialize)] // TODO add proper eq and ord
 pub struct NodeDto {
     name: String,
     addr: String,
     active: bool,
+    vdisks: Vec<VDiskDto>,
+}
+
+#[derive(Serialize)]
+pub struct VDiskDto {
+    id: u32,
+    replicas: Vec<ReplicaDto>,
+}
+
+#[derive(Serialize)]
+pub struct ReplicaDto {
+    disk: String,
+    path: String,
 }
 
 impl NodeDto {
-    fn new(name: String, addr: String, active: bool) -> Self {
-        NodeDto { name, addr, active }
+    fn new(node: &Node, active: bool) -> Self {
+        let vdisks = node
+            .vdisks
+            .iter()
+            .filter(|vd| vd.replicas.iter().any(|r| r.node == node.name))
+            .map(|vd| {
+                let replicas = vd
+                    .replicas
+                    .iter()
+                    .filter(|r| r.node == node.name)
+                    .map(|r| ReplicaDto {
+                        disk: r.disk.clone(),
+                        path: r.path.clone(),
+                    })
+                    .collect();
+                VDiskDto {
+                    id: vd.id,
+                    replicas,
+                }
+            })
+            .collect();
+        NodeDto {
+            name: node.name.clone(),
+            addr: node.address.to_string(),
+            active,
+            vdisks,
+        }
     }
 }
 
@@ -44,10 +82,9 @@ pub async fn get(cookie_jar: &CookieJar<'_>) -> Result<Template, Redirect> {
         let context = match nodes_from_bob {
             Ok(nodes) => {
                 let mut result = vec![];
-                for node in nodes {
-                    let active = is_active(&node).await.unwrap_or(false);
-                    let Node { name, address, .. } = node;
-                    result.push(NodeDto::new(name, address.to_string(), active));
+                for node in &nodes {
+                    let active = is_active(node).await.unwrap_or(false);
+                    result.push(NodeDto::new(node, active));
                 }
                 IndexContext::from_nodes(result)
             }
