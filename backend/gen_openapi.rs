dbe885a6ccbbbd390f786018763b393c1cafb058
Simeon Romanov <archeosanacritis@gmail.com>
2023-10-13 02:13:11 +0300

    remove json schema

--- backend/gen_openapi.rs
+++ backend/gen_openapi.rs
@@ -2,8 +2,6 @@ use std::fs;
 use utoipa::OpenApi;
 // in ./src/gen_openapi.rs
 fn main() {
-    let doc = backend::ApiDoc::openapi().to_pretty_json().unwrap();
-    let _ = fs::write("./api/openapi.json", doc);
     let doc = backend::ApiDoc::openapi().to_yaml().unwrap();
     let _ = fs::write("./api/openapi.yaml", doc);
 }
