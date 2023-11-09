99f5a6aa6f9430efeceadd1ad4a36ab4a6e6dca3
Simeon Romanov <archeosanacritis@gmail.com>
2023-11-09 07:59:26 +0300

    gen-openapi moved to utils

--- utils/gen_openapi.rs
+++ utils/gen_openapi.rs
@@ -1,6 +1,5 @@
 use std::fs;
 use utoipa::OpenApi;
-// in ./src/gen_openapi.rs
 fn main() {
     let doc = backend::ApiDoc::openapi().to_yaml().unwrap();
     let _ = fs::write("./api/openapi.yaml", doc);
