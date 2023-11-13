496fd1b7f7a9c074d76b5f99137aba18122c249c
Simeon Romanov <archeosanacritis@gmail.com>
2023-11-13 17:32:47 +0300

    build fix

--- utils/gen_openapi.rs
+++ utils/gen_openapi.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use utoipa::OpenApi;
 fn main() {
-    let doc = backend::ApiDoc::openapi().to_yaml().unwrap();
+    let doc = bob_management::ApiDoc::openapi().to_yaml().unwrap();
     let _ = fs::write("./api/openapi.yaml", doc);
 }
