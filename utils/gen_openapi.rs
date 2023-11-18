4bdef801b422a20cd8b88cf2e3dac3c1955999cb
Simeon Romanov <archeosanacritis@gmail.com>
2023-11-18 19:27:34 +0300

    post-review fixes

--- utils/gen_openapi.rs
+++ utils/gen_openapi.rs
@@ -1,6 +1,19 @@
 use std::fs;
 use utoipa::OpenApi;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Filename to save the OpenAPI schema
+    #[arg(short, long)]
+    filename: String,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let doc = bob_management::ApiDoc::openapi().to_yaml().unwrap();
-    let _ = fs::write("./api/openapi.yaml", doc);
+    let _ = fs::write(args.filename, doc).expect("Couldn't write schema to file");
 }
